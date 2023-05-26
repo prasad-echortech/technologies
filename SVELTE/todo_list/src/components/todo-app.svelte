61c4b8c62a579073336d24c30397631f11278dfe
prasad <prasad.s@echortech.com>
2023-05-26 18:52:39 +0530

    2/05/2023 work

--- SVELTE/todo_list/src/components/todo-app.svelte
+++ SVELTE/todo_list/src/components/todo-app.svelte
@@ -15,10 +15,7 @@
         toDoList.splice(i, 1);
         toDoList = toDoList;
     }
-   
 </script>
-
-
 
 <div style="margin:0 auto;padding:10px;width:400px">
     <h1 style="text-align:center;">ToDo-list</h1>
@@ -26,10 +23,9 @@
     <div style="display:flex">
         <input type="text" bind:value={textInput} />
         <button on:click={addToDo}>Add</button>
-        
-        
     </div>
 </div>
+
 {#each toDoList as toDo, i}
     <div style="display:flex; align-items:baseline;margin:0 auto; width:400px;">
         {#if toDo.editing}
